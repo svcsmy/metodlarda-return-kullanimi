be2bfa13c6847a517a3826cf1709b0b81f1ebb75
Sümeyye <58435258+svcsmy@users.noreply.github.com>
2023-08-12 23:12:32 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class main {
+	
+	public static int ikiilecarp(int a) {
+		return a*2;
+	}
+	
+	public static int ikiiletopla(int a) {
+		return a+2;
+	}
+	
+	public static int dörtilecarp(int a) {
+		return a*4;
+	}
+	
+	public static void main(String[] args) {
+ 
+	System.out.println(dörtilecarp(ikiiletopla(ikiilecarp(8))));
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
